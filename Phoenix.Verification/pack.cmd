d2846252ebf50dc662e3d2952c3f90cf21f6680e
Théophile Spyrou <theofilos.spyrou@gmail.com>
2022-08-27 12:00:46 +0300

    Update NuGet pack script

--- Phoenix.Verification/pack.cmd
+++ Phoenix.Verification/pack.cmd
@@ -1,27 +1,23 @@
+Rem Pack script for Windows Console
+
 @echo off
 
-echo If you haven't authenticated GitHub stream, then use the command below:
-echo dotnet nuget add source https://nuget.pkg.github.com/AskPhoenix/index.json -n AskPhoenix -u AskPhoenix -p GH_TOKEN
+echo *** NuGet Pack script ***
 echo.
-echo.
+
+echo Removing old packages...
+del ".\bin\Release\*.nupkg"
 
 echo Installing dependencies...
-echo.
-
 dotnet restore
 
 echo Building Release...
-echo.
-
 dotnet build --configuration Release --no-restore
 
 echo Packing Release...
-echo.
-
 dotnet pack --configuration Release --no-restore
 
-echo.
-echo.
+echo Pushing package...
 dotnet nuget push "**/*.nupkg" --source "AskPhoenix" --skip-duplicate
 
-Pause
+pause
